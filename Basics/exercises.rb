c1dee4641bbb0fc39403dd0f69074590131f5702
Robert Laws <roblaws@me.com>
2017-01-22 15:57:47 +0300

    beginning exercises

---
+++ Basics/exercises.rb
@@ -0,0 +1,13 @@
+# Exercises for The Basics
+
+# 1
+# firstname = "Bob"
+# lastname = "Smith"
+# puts firstname + " " + lastname
+
+"Harry " + "Henderson"
+
+number = 6284
+number.to_s.split('')
+puts number
+
